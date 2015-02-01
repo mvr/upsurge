922508b5c263d6912f560741607f2c65cbae13fb
Mitchell Riley <mitchell.v.riley@gmail.com>
2015-02-01 18:03:11 +1000

    Fix dependencies

--- Main.hs
+++ Main.hs
@@ -9,25 +9,25 @@ import GlossHelpers
 import Vec
 import Poly
 
-square :: Poly
-square = Poly [Vec 0 0, Vec 50 0, Vec 100 100, Vec 0 100]
+shape :: Poly
+shape = Poly [Vec 0 0, Vec 50 0, Vec 100 100, Vec 0 100]
 
 trans :: Vec -> Poly -> Poly
 trans v (Poly ps) = Poly $ fmap (+v) ps
 
-square2 :: Poly
-square2 = trans (Vec (-25) 25) square
+square :: Poly
+square = Poly [Vec 0 0, Vec 50 0, Vec 50 50, Vec 0 50]
 
-intersectingLine :: Line
-intersectingLine = Line (Vec (-50) 0) (Vec 1 1.5)
+square2 :: Poly
+square2 = trans (Vec (-20) (-20)) square
+
+reified = shunt (Vec 0 1) square2 shape
+
+levels = scanlevels shape square2
 
 foo :: [Vec]
-foo = collisionPoints $ fromJust $ collidePoly square square2
-
--- test :: [Poly]
--- test = [a, b]
---   where (a, b) = slice square (BP 0 0.5) (BP 2 0.5)
+foo = points reified
 
 main :: IO ()
-main = display (InWindow "Shunt" (600, 600) (300, 300)) white (polysPicture [square, square2] <> dots)
+main = display (InWindow "Shunt" (600, 600) (300, 300)) white (polysPicture [shape, reified, square2] <> dots)
   where dots = mconcat $ fmap dotPicture foo
